865a125d349c4377a9c90e0b03742e9313e98a73
arlsalsabella <aureliabella99@gmail.com>
2019-11-21 12:37:26 +0700

    my first commit

---
+++ precache-manifest.209fa2b849c54594534f152664d552e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "518687c25255139579b6",
+    "url": "/css/chunk-vendors.41ca90ca.css"
+  },
+  {
+    "revision": "a86e5b4954f84dc5a4f553206ca80683",
+    "url": "/index.html"
+  },
+  {
+    "revision": "419cbdcc6a410c97b44b",
+    "url": "/js/app.7b047c20.js"
+  },
+  {
+    "revision": "518687c25255139579b6",
+    "url": "/js/chunk-vendors.94423518.js"
+  },
+  {
+    "revision": "fd354749f258135991157cdfabae436a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
